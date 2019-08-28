c1d87a9db9250cdefbe7228adba3d41b7312b9f3
advaith <advaith.madhukar@gmail.com>
2019-08-28 17:52:26 +0530

    removed unsused folder

--- login/authentication_page/views.py
+++ login/authentication_page/views.py
@@ -39,7 +39,7 @@ def login (request):
             auth.login(request,user)
             return render(request, 'success.html')
         else:
-            messages.info(request,'invaild login')
+            messages.info(request,'invaild credentials')
             return redirect("/login")
     else:
         return render(request,'login.html')
